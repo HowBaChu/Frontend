866b28c2428a93525c7ab3114b1e022df88cefd6
hyunyeee <5004ohy@naver.com>
2023-08-29 22:19:06 +0900

    feat: 신고하기 모달 구현

---
+++ howbachu-frontend/src/assets/data/report_reasons_data.js
@@ -0,0 +1,12 @@
+const reportReasons = [
+  { title: "스팸 내용", detail: "무의미한 반복 게시물, 광고" },
+  { title: "욕설 및 혐오 발언", detail: "타인을 모욕하거나 비하하는 내용" },
+  { title: "부적절한 내용", detail: "성적, 폭력적 또는 민감한 내용 포함" },
+  { title: "개인 정보 유출", detail: "타인의 개인정보를 무단으로 공개한 경우" },
+  { title: "사기 또는 허위 정보", detail: "거짓 정보나 사기성 내용 공유" },
+  { title: "저작권 침해", detail: "타인의 저작물을 무단 도용한 경우" },
+  { title: "프로필 또는 아이디 부적절함", detail: "도발적, 선정적인 프로필" },
+  { title: "기타", detail: "위의 카테고리에 해당하지 않는 기타 이유로 신고" },
+];
+
+export default reportReasons;
