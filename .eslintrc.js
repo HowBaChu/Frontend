a1e2d782d95691f92e3388a22236f79a529ad7da
hyunyeee <5004ohy@naver.com>
2023-09-01 21:58:41 +0900

    setting: eslint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,29 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [    "react-app", // CRA 기본 설정 추가
+    "eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: [".eslintrc.{js,cjs}"],
-            parserOptions: {
-                sourceType: "script",
-            },
-        },
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: ["react"],
-    rules: {
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off",
-    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "no-unused-vars": "off",
+  },
 };
