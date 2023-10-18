376481bb8debebec1b08562ab438b0390f63ff6c
hyunyeee <5004ohy@naver.com>
2023-10-18 21:31:30 +0900

    fix: JSON 괄호 중첩 문제 해결

--- src/api/PostReport.js
+++ src/api/PostReport.js
@@ -1,9 +1,9 @@
 import { Axios } from "./Axios";
 
 export const PostReport = (reportData) => {
-  Axios.post(`/api/v1/report`, {
+  Axios.post(`/api/v1/report`,
     reportData,
-  })
+  )
     .then((res) => {
       console.log(res.data.data);
     })
