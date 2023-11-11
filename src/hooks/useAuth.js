66732e6e3b81841ff1ff476fc0916e295921732c
hyunyeee <5004ohy@naver.com>
2023-11-12 01:26:36 +0900

    feat: 로그인 후 MainPage로 redirection

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+
+export const useAuth = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = Cookies.get("Access-Token");
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const login = () => {
+    setIsLoggedIn(true);
+    console.log(isLoggedIn);
+  };
+
+  const logout = () => {
+    Cookies.remove("Access-Token");
+    setIsLoggedIn(false);
+  };
+
+  return { isLoggedIn, login, logout };
+};
+
+export default useAuth;
