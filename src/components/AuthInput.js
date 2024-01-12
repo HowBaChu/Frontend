5b7868395ef234776236aee588c63ecdad4dcce0
hyunyeee <5004ohy@naver.com>
2024-01-13 01:48:20 +0900

    feat: 유효성 검사 구현
    - react-hook-form, yup
    - 유효성 검사 스키마 추가
    - 에러 메세지 출력 및 UI 수정

--- src/components/AuthInput.js
+++ src/components/AuthInput.js
@@ -1,44 +1,62 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 import HiddenBtn from "./HiddenBtn";
 
 const AuthInput = ({
   inputId,
   label,
+  name,
   type,
   placeholder,
   toggleEye,
   eyeState,
   value,
-  onChange,
+  register,
+  errorMsg,
 }) => {
   let hasHiddenBtn = ["password", "checkPwd"].includes(inputId);
+
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsError(errorMsg !== undefined);
+  }, [errorMsg]);
+
   return (
-    <StyledInput>
-      <Label htmlFor={inputId}>{label}</Label>
-      <InputWrapper>
-        <Input
-          id={inputId}
-          type={type}
-          placeholder={placeholder}
-          value={value}
-          {...(onChange && { onChange: (e) => onChange(e) })}
-        />
-        {hasHiddenBtn && (
-          <HiddenBtn toggleEye={toggleEye} eyeState={eyeState} />
-        )}
-      </InputWrapper>
-    </StyledInput>
+    <>
+      <StyledInput $isError={isError}>
+        <Label htmlFor={inputId} $isError={isError}>
+          {label}
+        </Label>
+        <InputWrapper>
+          <Input
+            id={inputId}
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            value={value[name]}
+            {...register(name)}
+          />
+          {hasHiddenBtn && (
+            <HiddenBtn toggleEye={toggleEye} eyeState={eyeState} />
+          )}
+        </InputWrapper>
+      </StyledInput>
+      <HelperTxt $isError={isError}>{errorMsg && errorMsg}</HelperTxt>
+    </>
   );
 };
 
 const StyledInput = styled.div`
-  width: 327px;
+  width: 100%;
   height: 60px;
   padding: 10px 13px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  border: 1px solid ${({ theme }) => theme.colors.LIGHT_GRAY};
+  border: 1px solid
+    ${({ theme, $isError }) =>
+      $isError ? theme.colors.TOMATO : theme.colors.LIGHT_GRAY};
   border-radius: 8px;
 `;
 const InputWrapper = styled.div`
@@ -48,16 +66,24 @@ const InputWrapper = styled.div`
 const Label = styled.label`
   font-size: ${({ theme }) => theme.fontsize.SMALL_TXT};
   font-weight: ${({ theme }) => theme.fontweight.REGULAR};
-  color: ${({ theme }) => theme.colors.LIGHT_GRAY};
+  color: ${({ theme, $isError }) =>
+    $isError ? theme.colors.TOMATO : theme.colors.TXT_GRAY};
 `;
 const Input = styled.input`
   width: 100%;
-  font-size: ${({ theme }) => theme.fontsize.DEFAULT};
+  font-size: ${({ theme }) => theme.fontsize.S_TOPIC_TITLE};
   border: none;
-
+  color: ${({ theme }) => theme.colors.DARK_GRAY};
   &::placeholder {
-    color: ${({ theme }) => theme.colors.GRAY};
+    color: ${({ theme }) => theme.colors.TXT_GRAY};
   }
+`;
+const HelperTxt = styled.p`
+  width: 100%;
+  margin-left: 10px;
+  line-height: 16px;
+  font-size: ${({ theme }) => theme.fontsize.SMALL_TXT};
+  color: ${({ theme, $isError }) => $isError && theme.colors.TOMATO};
 `;
 
 export default AuthInput;
