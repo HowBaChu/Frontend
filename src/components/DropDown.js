fcdee3a9fac2bd2b6e95320778d642b3643b556a
hyunyeee <5004ohy@naver.com>
2024-01-20 02:17:46 +0900

    feat: 회원가입 페이지 MBTI 드롭다운으로 변경

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -4,7 +4,6 @@ import OPTION_DATA from "../assets/data/option_data";
 const DropDown = ({ mbti, onValueChange }) => {
   return (
     <InfoInputBox>
-      <InputTitle>MBTI</InputTitle>
       <InputContainer>
         <Select
           name="mbti"
@@ -21,18 +20,11 @@ const DropDown = ({ mbti, onValueChange }) => {
     </InfoInputBox>
   );
 };
-
 const InfoInputBox = styled.div`
   width: 100%;
 
   flex-direction: column;
   align-items: start;
-`;
-const InputTitle = styled.div`
-  margin-bottom: 2px;
-  font-size: ${({ theme }) => theme.fontsize.B_TOPIC_TITLE};
-  font-weight: ${({ theme }) => theme.fontweight.SEMIBOLD};
-  color: ${({ theme }) => theme.colors.PURPLE3};
 `;
 const InputContainer = styled.div`
   height: ${({ textArea }) => (textArea ? `120px` : `40px`)};
@@ -44,7 +36,6 @@ const InputContainer = styled.div`
   overflow: hidden;
   background-color: white;
 `;
-
 const Select = styled.select`
   margin-bottom: 10px;
   margin-top: 10px;
